c47174366768f4da4810d9b68ef89e4746f53548
tugascrown204 <gajdertevet@hotmail.com>
2025-09-26 15:10:38 +0330

    feat: initial commit for exercises/refactoring.js

---
+++ exercises/refactoring.js
@@ -0,0 +1,10 @@
+// Refactoring Exercise
+// Example of poor naming that needs to be improved
+function c(n) {
+    return n * n;
+}
+
+// Improve the code with better naming
+function calculateSquare(number) {
+    return number * number;
+}
