67b133e2f746772e2d8df3c9a6ad95ecabd78dbb
tugascrown204 <gajdertevet@hotmail.com>
2025-09-26 15:10:41 +0330

    feat: initial commit for exercises/commenting.java

---
+++ exercises/commenting.java
@@ -0,0 +1,16 @@
+// Commenting Exercise
+
+/* This class represents a Circle */
+public class Circle {
+    private double radius;
+
+    /* Constructor to create a Circle with a specific radius */
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    /* Method to calculate the area of the Circle */
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
